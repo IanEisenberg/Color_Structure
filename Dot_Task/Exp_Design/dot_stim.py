0b7873c9ef3e7929064f8aaeee026cbe8747fcfd
Ian <ianeisenberg90@gmail.com>
2017-02-01 01:25:05 -0800

    added two color stim

--- Dot_Task/Exp_Design/dot_stim.py
+++ Dot_Task/Exp_Design/dot_stim.py
@@ -6,8 +6,17 @@ pyglet.options['debug_gl'] = False
 import ctypes
 GL = pyglet.gl
 import numpy as np
+from skimage.color import lab2rgb
 
+def pixel_lab2rgb(lst):
+    lst = [float(x) for x in lst]
+    return lab2rgb([[(lst)]]).flatten()
+    
+    
 class ColorDotStim(DotStim):
+    """
+    Motion/Color Dot Stim
+    """
     def __init__(self, win, color_proportion, **kwargs):
         self.color_proportion = color_proportion
         super(ColorDotStim, self).__init__(win = win, **kwargs)
@@ -77,6 +86,10 @@ class ColorDotStim(DotStim):
 
         
 class ColorDensityStim(DotStim):
+    """
+    Color dot stim with an outer ring that can be more/less dense or have higher/
+    lower contrast
+    """
     def __init__(self, win, color_proportion, outer_proportion, outer_opacity, **kwargs):
         self.color_proportion = color_proportion
         self.outer_proportion = outer_proportion
@@ -219,7 +232,104 @@ class ColorDensityStim(DotStim):
         
         
         
-def getDotStim(win, motion_coherence = .5, color_proportion = .5, direction = 0, colors = None):
+class TwoColorStim(DotStim):
+    """
+    Color dot stim with an outer ring that can be more/less dense or have higher/
+    lower contrast. Expects colors in lab format
+    """
+    def __init__(self, win, color_proportions, colors, **kwargs):
+        assert len(np.array(colors).flatten()) == 12, \
+            "Must specify 4 lab colors"
+        self.colors = np.array(colors)
+        self.color_proportions = np.array(color_proportions)
+        assert not np.any(abs(self.color_proportions-1)>1), \
+            "Color proportions must be between 0 and 1"   
+        super(TwoColorStim, self).__init__(win = win, **kwargs)
+        
+
+            
+    def _update_dotsXY(self):
+        """The user shouldn't call this - its gets done within draw().
+        """
+        dots = self._newDotsXY(self.nDots)
+        self._verticesBase = dots
+        # update the pixel XY coordinates in pixels (using _BaseVisual class)
+        self._updateVertices()
+        
+    def draw(self, win=None):
+        """Draw the stimulus in its relevant window. You must call
+        this method after every MyWin.flip() if you want the
+        stimulus to appear on that frame and then update the screen again.
+        """
+        if win is None:
+            win = self.win
+        self._selectWindow(win)
+
+        self._update_dotsXY()
+
+        GL.glPushMatrix()  # push before drawing, pop after
+
+        # draw the dots
+        if self.element is None:
+            win.setScale('pix')
+            GL.glPointSize(self.dotSize)
+            
+            # load Null textures into multitexteureARB - they modulate with
+            # glColor
+            GL.glActiveTexture(GL.GL_TEXTURE0)
+            GL.glEnable(GL.GL_TEXTURE_2D)
+            GL.glBindTexture(GL.GL_TEXTURE_2D, 0)
+            GL.glActiveTexture(GL.GL_TEXTURE1)
+            GL.glEnable(GL.GL_TEXTURE_2D)
+            GL.glBindTexture(GL.GL_TEXTURE_2D, 0)
+
+            CPCD = ctypes.POINTER(ctypes.c_double)
+            GL.glVertexPointer(2, GL.GL_DOUBLE, 0,
+                               self.verticesPix.ctypes.data_as(CPCD))
+            # set colors
+            colors = []
+            for i,dimension in enumerate(self.colors):
+                dimension_color = dimension[0]*(self.color_proportions[i]) \
+                                + dimension[1]*(1-self.color_proportions[i])
+                dimension_color = [list(pixel_lab2rgb(dimension_color))]
+                colors += dimension_color*(self.nDots/2)
+            colors = np.array(colors).astype(ctypes.c_float)
+            
+            colors_gl = colors.ctypes.data_as(ctypes.POINTER(ctypes.c_float))
+            GL.glColorPointer(3, GL.GL_FLOAT,0, colors_gl)
+            GL.glEnableClientState(GL.GL_COLOR_ARRAY)
+            
+            #back to default code
+            GL.glEnableClientState(GL.GL_VERTEX_ARRAY)
+            GL.glDrawArrays(GL.GL_POINTS, 0, self.nDots)
+            GL.glDisableClientState(GL.GL_VERTEX_ARRAY)
+            GL.glDisableClientState(GL.GL_COLOR_ARRAY)
+            
+        else:
+            # we don't want to do the screen scaling twice so for each dot
+            # subtract the screen centre
+            initialDepth = self.element.depth
+            for pointN in range(0, self.nDots):
+                _p = self.verticesPix[pointN, :] + self.fieldPos
+                self.element.setPos(_p)
+                self.element.draw()
+            # reset depth before going to next frame
+            self.element.setDepth(initialDepth)
+        GL.glPopMatrix()
+
+    def setColorProportion(self, val, op='', log=None):
+        """Usually you can use 'stim.attribute = value' syntax instead,
+        but use this method if you need to suppress the log message
+        """
+        val = np.array(val)
+        assert not np.any(abs(val-1)>1), \
+            "Color proportions must be between 0 and 1"
+        setAttribute(self, 'color_proportions', val, log, op)   
+        
+
+# access methods
+
+def getColorDotStim(win, motion_coherence = .5, color_proportion = .5, direction = 0, colors = None):
     if colors == None:
         colors = [(1.0,0.0,0.0), (0.0,0.8,0.8)]
     dots = ColorDotStim(win, color_proportion, nDots = 500, dotSize = 4, signalDots = 'different', fieldShape = 'circle',
@@ -227,7 +337,7 @@ def getDotStim(win, motion_coherence = .5, color_proportion = .5, direction = 0,
                           color = colors, opacity = 1)
     return dots
 
-def getDensityStim(win, color_proportion=.5, outer_proportion=1.8, 
+def getColorDensityStim(win, color_proportion=.5, outer_proportion=1.8, 
                    opacity=1, outer_opacity=1, colors = None):
     if colors == None:
         colors = [(1.0,0.0,0.0), (0.0,0.8,0.8)]
@@ -238,9 +348,16 @@ def getDensityStim(win, color_proportion=.5, outer_proportion=1.8,
                             outer_opacity = outer_opacity)
     return dots
     
-    
-    
-    
+
+def getTwoColorStim(win, color_proportions=[.5,.5], 
+                   opacity=1, colors = None):
+    if colors == None:
+        colors = [[(80,100,80), (80,-100,80)], [(80,0,100), (80,0,-100)]]
+    dots = TwoColorStim(win, colors=colors, color_proportions=color_proportions,
+                        nDots=1000, dotSize=4, signalDots='different', 
+                        fieldShape='circle',fieldSize=15)
+    return dots
+        
 def display_stim(win, stim, n):
     for _ in range(n):
         stim.draw()
@@ -252,40 +369,7 @@ def display_stim(win, stim, n):
     
 win = visual.Window([1200,800], color = [-.8,-.8,-.8], allowGUI=False, fullscr=False, 
                                      monitor='testMonitor', units='deg')    
-a = getDensityStim(win, outer_proportion=.6, opacity=.7)
+a = getTwoColorStim(win, color_proportions = [0,0])
 display_stim(win,a,50)
 win.close()
-
-
-
-# example display of dot stim
-"""
-def display_stim(win, stim, n):
-    for _ in range(n):
-        stim.draw()
-        win.flip()
-        keys = event.getKeys(keyList=['s', 'l'])
-        if keys != []:
-            break
-    core.wait(.015)
-        
-def play():
-    win = visual.Window([1200,800], color = [-.8,-.8,-.8], allowGUI=False, fullscr=False, 
-                                     monitor='testMonitor', units='deg')
-    directions = [i%360 for i in range(720)]
-    coherence = [(0.0+i)/(len(directions)-1) for i in range(len(directions))]
-    dots = getDotStim(win, motion_coherence = .5, color_proportion = 0)
-    
-    display_stim(win,dots,50)
-    for d,c in zip(directions,coherence):
-        dots.draw()
-        win.flip()
-        core.wait(.015)
-        dots.setDir(d)
-        dots.setColorCoherence(c)
-    display_stim(win,dots,50)
-    
-    win.close()
-    return dots
-dots = play()
-"""
+      
