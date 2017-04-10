77aed15fe57effde2d5b52dd2e7a3d8a4a0bfca7
Ian <ianeisenberg90@gmail.com>
2017-04-10 14:10:34 -0700

    added params to util function

--- Color_Shape_Task_V2/Exp_Design/util.py
+++ Color_Shape_Task_V2/Exp_Design/util.py
@@ -8,7 +8,7 @@ Created on Thu Feb 16 14:58:26 2017
 
 import numpy as np
 
-def setBeta(seed = None):
+def setBeta(seed = None, ab=None):
     """
     Sets a random beta distribution from a pre-defined catalog
     ------------------------------
@@ -17,10 +17,13 @@ def setBeta(seed = None):
     """
     if seed is not None:
         np.random.seed(seed)
-    beta_catalog = [[20, 21],[2, 4],[3, 23],[1, 15]]
-    rand_num = np.random.randint(0,len(beta_catalog)-1)
-    beta_ab = beta_catalog[rand_num]
-    np.random.shuffle(beta_ab)
+    if ab is None:
+        beta_catalog = [[20, 21],[2, 4],[3, 23],[1, 15]]
+        rand_num = np.random.randint(0,len(beta_catalog)-1)
+        beta_ab = beta_catalog[rand_num]
+        np.random.shuffle(beta_ab)
+    else: 
+        beta_ab = ab
     # center & scale the beta distribution from 0-1 to -1 - 1 
     loc = -1
     scale = 2
