ca070639ce9af8090dac27f05fbef25ea7809af9
ian <ianeisenberg90@gmail.com>
2018-03-11 17:53:11 -0700

    fixed bug to load most recent trackers/recently

--- Dot_Task/Exp_Design/utils.py
+++ Dot_Task/Exp_Design/utils.py
@@ -11,17 +11,19 @@ import os
 def get_difficulties(subject_code):
     file_dir = os.path.dirname(__file__)
     try:
-        motion_file = glob(os.path.join(file_dir,'..','Data','RawData',
-                                        '*%s*motion*' % subject_code))[-1]
+        motion_file = sorted(glob(os.path.join(file_dir,'..','Data','RawData',
+                                        '*%s*motion*' % subject_code)))[-1]
         motion_data = cPickle.load(open(motion_file,'r'))
         motion_difficulties = {k:v.mean() for k,v in motion_data['trackers'].items()}
+        print('Found Motion Difficulties. Loading from file: %s' % motion_file)
     except IndexError:
         motion_difficulties = {}
     try:
-        orientation_file = glob(os.path.join(file_dir,'..','Data','RawData',
-                                       '*%s*orientation*' % subject_code))[-1]
+        orientation_file = sorted(glob(os.path.join(file_dir,'..','Data','RawData',
+                                       '*%s*orientation*' % subject_code)))[-1]
         orientation_data = cPickle.load(open(orientation_file,'r'))
         orientation_difficulties = {k:v.mean() for k,v in orientation_data['trackers'].items()}
+        print('Found Orientation Difficulties. Loading from file: %s' % orientation_file)
     except IndexError:
         orientation_difficulties = {}
     return motion_difficulties, orientation_difficulties
@@ -29,16 +31,16 @@ def get_difficulties(subject_code):
 def get_trackers(subject_code):
     file_dir = os.path.dirname(__file__)
     try:
-        motion_file = glob(os.path.join(file_dir,'..','Data','RawData',
-                                        '*%s*motion*' % subject_code))[-1]
+        motion_file = sorted(glob(os.path.join(file_dir,'..','Data','RawData',
+                                        '*%s*motion*' % subject_code)))[-1]
         motion_data = cPickle.load(open(motion_file,'r'))
         motion_trackers = motion_data['trackers']
         print('Found Motion Trackers. Loading from file: %s' % motion_file)
     except IndexError:
         motion_trackers = {}
     try:
-        orientation_file = glob(os.path.join(file_dir,'..','Data','RawData',
-                                       '*%s*orientation*' % subject_code))[-1]
+        orientation_file = sorted(glob(os.path.join(file_dir,'..','Data','RawData',
+                                       '*%s*orientation*' % subject_code)))[-1]
         orientation_data = cPickle.load(open(orientation_file,'r'))
         orientation_trackers = orientation_data['trackers']
         print('Found Orientation Trackers. Loading from file: %s' % orientation_file)
